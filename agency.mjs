ebed76172bef40c3ebd0d1e15b683528d19422c1
JerPatterson <jeremie.patterson@polymtl.ca>
2023-06-12 13:15:44 -0400

    agency file reconfig

--- agency.mjs
+++ agency.mjs
@@ -1,33 +1,19 @@
 import fetch from 'node-fetch'
-import fs from 'fs'
+import { readFile } from './file.mjs';
 
-const LINE_DELIMITER = '\r\n';
-const VALUE_DELIMITER = ',';
+export async function agencyPopulate() {
+    const resAgency = await readFile("./assets/agency.txt", "agency_id");
+    resAgency.pop();
 
-async function readFile(path, firstParam) {
-    const content = fs.readFileSync(path).toLocaleString();
-    const parameters = content.split(LINE_DELIMITER, 1)[0].split(VALUE_DELIMITER);
-    parameters[0] = firstParam;
-
-    return content.split(LINE_DELIMITER).slice(1).map((line) => {
-        const result = Object();
-        line.split(VALUE_DELIMITER).forEach((value, index) => {
-        result[parameters[index]] = value;
+    resAgency.forEach(async (agency) => {
+        const response = await fetch('http:/127.0.0.1:3000/agencies', {
+            method: 'PUT',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(agency)
         });
-        return result;
-    });
+
+        console.log(response.status);
+    })
 }
 
-const resAgency = await readFile("./assets/agency.txt", "agency_id");
-resAgency.pop();
-
-resAgency.forEach(async (agency) => {
-    const response = await fetch('http:/127.0.0.1:3000/agencies', {
-        method: 'PUT',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify(agency)
-    });
-
-    console.log(response);
-})
-
+await agencyPopulate();
